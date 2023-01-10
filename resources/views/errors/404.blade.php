1eadb38a754653a547c77dd274255ab7ac6acf4d
manuelmenama <manuel.mena090589@gmail.com>
2023-01-10 16:18:00 +0100

    first

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.errors')
+
+@section('content')
+
+<div class="container">
+    <h1>ERROR 404</h1>
+</div>
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+<header>
+    <nav class="d-flex align-items-center justify-content-between">
+        <h2>LOGO</h2>
+
+        <ul class="d-flex align-items-center">
+            <li class="me-5">
+                <a href="">Link</a>
+            </li>
+            <li class="me-5">
+                <a href="">Link</a>
+            </li>
+            <li class="me-5">
+                <a href="">Link</a>
+            </li>
+        </ul>
+
+    </nav>
+</header>
