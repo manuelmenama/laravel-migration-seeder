bb2e61b6e97fdda0d970a31f8f649841fe9569e3
manuelmenama <manuel.mena090589@gmail.com>
2023-01-10 18:31:07 +0100

    seeding almost gone

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use Faker\Generator as Faker;
+
 use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,8 +15,23 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
+        for($i=0; $i<10; $i++){
+            $new_train = new Train();
+            $new_train->company = 'trenitalia';
+            $new_train->from = $faker->city();
+            $new_train->to = $faker->city();
+            $new_train->departure = $faker->time();
+            $new_train->arrival = $faker->time();
+            $new_train->train_number = 12345;
+            $new_train->total_couch = $faker->numberBetween(6, 20);
+            $new_train->is_on_time = 1;
+            $new_train->is_deleted = 0;
+            //dump($new_train);
+            $new_train->save();
+        }
+
         /*
             $table->string('company', 15);
             $table->string('from', 30);
@@ -27,18 +44,19 @@ class TrainsTableSeeder extends Seeder
             $table->tinyInteger('is_deleted')->default(0);
             $table->timestamps();
         */
-        $new_train = new Train();
-        $new_train->company = 'Italo';
-        $new_train->from = 'Roma Termini';
-        $new_train->to = 'Milano Centrale';
-        $new_train->departure = '10:00:00';
-        $new_train->arrival = '13:00:00';
-        $new_train->train_number = 12345;
-        $new_train->total_couch = 6;
-        $new_train->total_couch = 1;
-        $new_train->total_couch = 0;
-        $new_train->save();
-
+        /*
+            $new_train = new Train();
+            $new_train->company = 'Italo';
+            $new_train->from = 'Roma Termini';
+            $new_train->to = 'Milano Centrale';
+            $new_train->departure = '10:00:00';
+            $new_train->arrival = '13:00:00';
+            $new_train->train_number = 12345;
+            $new_train->total_couch = 6;
+            $new_train->is_on_time = 1;
+            $new_train->is_deleted = 0;
+            $new_train->save();
+        */
 
 
 
